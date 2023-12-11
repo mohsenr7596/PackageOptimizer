cd0c0508f602f17f90b7a36ac68b6a7a9832eab2
Mohsen <mohsenr7596@hotmail.com>
2023-12-11 20:40:19 +0330

    add number to product

--- access/src/main/java/com/example/packer/domain/Product.java
+++ access/src/main/java/com/example/packer/domain/Product.java
@@ -19,14 +19,26 @@ public class Product {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column(name = "number")
+    private Short number;
+
     @Column(name = "weight", nullable = false)
     private double weight;
 
     @Column(name = "price", nullable = false)
-    private double price;
+    private int price;
+
+    @Transient
+    private transient Double ratio;
 
 
-    public Product(double weight, double price) {
+    public Product(double weight, int price) {
+        this.weight = weight;
+        this.price = price;
+    }
+
+    public Product(Short number, double weight, int price) {
+        this.number = number;
         this.weight = weight;
         this.price = price;
     }
@@ -37,6 +49,33 @@ public class Product {
         if (!(o instanceof Product product)) return false;
 
         if (Double.compare(weight, product.weight) != 0) return false;
-        return Double.compare(price, product.price) == 0;
+        return price == product.price;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(weight);
+        result = (int) (temp ^ (temp >>> 32));
+        result = 31 * result + price;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+               "number=" + number +
+               ", weight=" + weight +
+               ", price=" + price +
+               '}';
+    }
+
+    @Transient
+    public double getRatio() {
+        if (ratio == null) {
+            ratio = price / weight;
+        }
+        return ratio;
     }
 }
